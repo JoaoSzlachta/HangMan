48e12c85407b51e98b271a7fd66a534f7bfdfcd3
JoaoSzlachta <jpbs2002@gmail.com>
2025-02-18 11:42:32 -0300

    feat: start word guessing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+import words from "./assets/wordList.json"
+import Heading from './components/heading/heading.tsx'
 
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   return (
     <>
-
+      <Heading />
     </>
   )
 }
