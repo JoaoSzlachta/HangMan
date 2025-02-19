19c54408c17299bd28afe537baab583825f33685
JoaoSzlachta <jpbs2002@gmail.com>
2025-02-19 14:05:42 -0300

    style: add hanged man body parts

---
+++ src/components/hangman_drawing/body_parts.tsx
@@ -0,0 +1,27 @@
+import './body_parts.css'
+
+const head = (
+    <div className="bodyParts__head"/>
+);
+
+const body = (
+    <div className="bodyParts__body"/>
+);
+
+const leftArm = (
+    <div className="bodyParts__leftArm"/>
+);
+
+const rightArm = (
+    <div className="bodyParts__rightArm"/>
+);
+
+const leftLeg = (
+    <div className="bodyParts__leftLeg"/>
+);
+
+const rightLeg = (
+    <div className="bodyParts__rightLeg"/>
+);
+
+export { head, body, rightArm, leftArm, leftLeg, rightLeg};
--- src/components/hangman_drawing/hangman_drawing.tsx
+++ src/components/hangman_drawing/hangman_drawing.tsx
@@ -1,8 +1,15 @@
 import "./hangman_drawing.css"
+import { head, body, rightArm, leftArm, rightLeg, leftLeg} from './body_parts'
 
 export default function HangmanDrawing () {
     return (
         <div className="drawing--container">
+            {head}
+            {body}
+            {rightArm}
+            {leftArm}
+            {rightLeg}
+            {leftLeg}
             <div className="drawing__gallow--rope" />
             <div className="drawing__gallow--bracket" />
             <div className="drawing__gallow--pole" />
